02db2cea8058ab45a4a01734ec63631022c4d914
Pranshu Pranjal <pranshupranjal321@gmail.com>
2025-08-31 11:45:16 +0000

    Chapter 2 b

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("The value is", aNumber)
+	if err == nil {
+		fmt.Println("String Length:", stringLength)
+	}
+	fmt.Printf("Value of number: %v %v\n", aNumber, str1)
+	fmt.Printf("Data type: %T\n", aNumber)
 }
